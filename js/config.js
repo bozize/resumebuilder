517ab9c262a09afd521cdf713a8e85e721756fe0
boziz <bozize@gmail.com>
2025-07-18 02:00:11 +0300

    fix

--- js/config.js
+++ js/config.js
@@ -1,7 +1,6 @@
-
-
-// config.js
+// Configuration for Supabase
 export const SUPABASE_CONFIG = {
-  url: process.env.VITE_SUPABASE_URL,
-  anonKey: process.env.VITE_SUPABASE_ANON_KEY
+  url: "https://roxcxgfjrjsrheqbtmho.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJveGN4Z2ZqcmpzcmhlcWJ0bWhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzOTQwODksImV4cCI6MjA2Njk3MDA4OX0.ZzVHGF9w4I27YG2Kkmv-MML5Ic1pVWUG6QvTCvkIbSM"
 };
+
